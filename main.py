47ea86094a9af73fc686cb4cc3ffc8c04a42d3a6
Levi <118043279+zvxx@users.noreply.github.com>
2023-04-22 16:21:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def Lev():
+
+    email = request.args.get('email')
+
+    url = "https://api2-19-h2.musical.ly/aweme/v1/passport/find-password-via-email/?app_language=en&manifest_version_code=2018101933&_rticket=1656747775754&iid=7115676682581247750&channel=googleplay&language=en&fp=&device_type=SM-A022F&resolution=720*1471&openudid=8c05dec470c7b7d5&update_version_code=2018101933&sys_region=IQ&os_api=30&is_my_cn=0&timezone_name=Asia%2FBaghdad&dpi=280&carrier_region=IQ&ac=wifi&device_id=7023349253125604869&mcc_mnc=41805&timezone_offset=10800&os_version=11&version_code=880&carrier_region_v2=418&app_name=musical_ly&ab_version=8.8.0&version_name=8.8.0&device_brand=samsung&ssmix=a&pass-region=1&build_number=8.8.0&device_platform=android&region=SA&aid=1233&ts=1656747775&as=a1e67fbb4fffb246cf0244&cp=f2f02d6bfbffb36de1emw&mas=01cd120efcb179ac1b331e5cecb80282052c2c4c0c66c66c2c4c46"
+
+    headers = {
+
+            'host':'api2-19-h2.musical.ly',
+
+            'connection':'keep-alive',
+
+            'cookie':'sstore-idc=maliva; store-country-code=iq; odin_tt=056f31c10f8c82638f6d4d64669ad49e9c36d4946d5d596f433d7f2d75fa1592a21c201d712196d54ee4ae4e14ac8708eee32dc97c85c0a65510024ecc0698346f73ecab038b7160dbff96ced716b8af',
+
+            'accept-Encoding':'gzip',
+
+             'user-agent':'com.zhiliaoapp.musically/2018101933 (Linux; U; Android 11; ar_IQ; SM-A022F; Build/RP1A.200720.012; Cronet/58.0.2991.0)',
+
+              'connection': 'close',}
+
+    data = f"app_language=en&manifest_version_code=2018101933&_rticket=1656747775754&iid=7115676682581247750&channel=googleplay&language=en&fp=&device_type=SM-A022F&resolution=720*1471&openudid=8c05dec470c7b7d5&update_version_code=2018101933&sys_region=IQ&os_api=30&is_my_cn=0&timezone_name=Asia%2FBaghdad&dpi=280&email={email}&retry_type=no_retry&carrier_region=IQ&ac=wifi&device_id=7023349253125604869&mcc_mnc=41805&timezone_offset=10800&os_version=11&version_code=880&carrier_region_v2=418&app_name=musical_ly&ab_version=8.8.0&version_name=8.8.0&device_brand=samsung&ssmix=a&pass-region=1&build_number=8.8.0&device_platform=android&region=SA&aid=1233"
+
+    Levi = requests.post(url,headers=headers,data=data).text
+
+    return(Levi)
